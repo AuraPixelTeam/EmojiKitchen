0eb869de3caec88401628a2f16265cd800243e32
Harry Nguyen <nguyenhieu.c47s@gmail.com>
2024-07-10 08:05:47 +0700

    Config package.json

--- dist/utils/EmojiUtils.js
+++ dist/utils/EmojiUtils.js
@@ -1,7 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.emojiToUnicode = emojiToUnicode;
-function emojiToUnicode(emoji) {
+export function emojiToUnicode(emoji) {
     const codePoints = [];
     for (const char of emoji) {
         codePoints.push('u' + char.codePointAt(0).toString(16).toUpperCase());
